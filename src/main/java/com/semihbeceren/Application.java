92ac3488f53009ed262e0102ba3c12c011e85c63
Semih Beceren <semih.beceren@hotmail.com>
2016-11-15 16:55:22 +0300

    Revert Add Outh-2 Annotation

--- src/main/java/com/semihbeceren/Application.java
+++ src/main/java/com/semihbeceren/Application.java
@@ -2,7 +2,6 @@ package com.semihbeceren;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,7 +13,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableCaching
 @EnableScheduling
 @EnableAsync
-@EnableOAuth2Sso
 public class Application
 {
     public static void main(String[] args) throws Exception {
