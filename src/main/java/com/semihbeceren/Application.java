f249ac2cbd783b160548db70737586e6aedca756
Semih Beceren <semih.beceren@hotmail.com>
2016-11-15 16:33:17 +0300

    Add Outh-2 Annotation

--- src/main/java/com/semihbeceren/Application.java
+++ src/main/java/com/semihbeceren/Application.java
@@ -2,6 +2,7 @@ package com.semihbeceren;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableCaching
 @EnableScheduling
 @EnableAsync
+@EnableOAuth2Sso
 public class Application
 {
     public static void main(String[] args) throws Exception {
